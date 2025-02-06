2e1917151d1d96ecde6ee850154358e7265c6489
finteger <nashassociatesinc@gmail.com>
2025-02-06 15:22:17 -0700

    Update

--- app.js
+++ app.js
@@ -12,6 +12,37 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+app.post('/api/chat-stream', async (req, res) =>{
+  try {
+    const { prompt } = req.body;
+
+    const stream = await ollama.chat({
+        model: 'deepseek-r1:7b',
+        messages : [{role: 'user', content: prompt}],
+        stream: true
+    });
+
+    res.setHeader('Content-type', 'text/plain');
+
+    for await (const chunk of stream){
+            //console.log(chunk.message)
+        try {
+            if(chunk.message?.content){
+                res.write(chunk.message.content)
+            }
+        } catch (streamError) {
+            console.error('Error:', streamError);
+            break;
+        }
+    }
+
+    res.end();
+    
+  } catch (error) {
+    
+  }
+});
+
 
 app.listen(PORT, () =>{
     console.log(`Running on port ${PORT}`);
